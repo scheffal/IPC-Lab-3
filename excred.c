c769c70eb4b8387772488627d5bf0de4252e9bb5
anthonyrblanton <anthonyrblanton@gmail.com>
2018-09-19 12:42:46 -0400

    seems to work

--- excred.c
+++ excred.c
@@ -3,18 +3,7 @@
 #include <unistd.h> 
 #include <string.h>
 #include <signal.h>
-
-void sigHandler(int sigNum)
-{
-	if(sigNum == SIGUSR1)
-	{
-		printf("received a SIGUSR1 signal\n");
-	}
-	if(sigNum == SIGUSR2)
-	{
-		printf("received a SIGUSR2 signal\n");
-	}
-}
+#include <time.h>
 
 void sigHandlerExit(int sigNum)
 {
@@ -22,58 +11,91 @@ void sigHandlerExit(int sigNum)
 	exit(0);
 }
 
+/**
+ * From https://www.linuxprogrammingblog.com/code-examples/sigaction
+ * and the man pages
+ */
+void sighandler(int signum, siginfo_t *siginfo, void *content) {
+
+    char *sigtype;
+
+    switch (signum) {
+        case SIGUSR1: sigtype = "SIGUSR1"; break;
+        case SIGUSR2: sigtype = "SIGUSR2"; break;
+        default: sigHandlerExit(signum);
+    }
+
+    printf("Received a %s from %d\n", sigtype, siginfo->si_pid);
+
+	
+}
+
+void sendsignal(int ppid) {
+    // wait a random amount of time between 1 and 5 seconds
+    int sleepytime = rand() % 5 + 1;
+    sleep(sleepytime);
+
+    // generate a random signal
+    int signumb = rand() % 2;
+    switch (signumb) {
+        case 0: kill(ppid, SIGUSR1); break;
+        case 1: kill(ppid, SIGUSR2);
+    }
+}
+
 int main() {
 
-    pid_t pid, pid2;
-	int sleepTime;
-	int randSig;
+    // seed the random number generator
+    srand(time(0));
 
-    // need to do this for each sigusr
-    // then install signal
+    // pid of processes
+    pid_t pid, pid2, ppid = getpid();
+
+    // register the handlers
     struct sigaction act;
-
-    act.sa_handler = sigHandler;
-
+    act.sa_sigaction = sighandler;
+    act.sa_flags = SA_SIGINFO;
     sigaction(SIGUSR1, &act, NULL);
     sigaction(SIGUSR2, &act, NULL);
-
-    // fork X2
+    
+    // spawn child processes
     if ((pid = fork()) < 0) {
         perror("Fork 1 failed");
         exit(1);
-    } 
-    if (pid) { // parent process
-        if (pid2 = fork() < 0) {
+    }
+
+    if (pid) {
+        if ((pid2 = fork()) < 0) {
             perror("Fork 2 failed");
             exit(1);
         }
-
-        while (1) {
-            struct sigaction act2;
-            act2.sa_handler = sigHandlerExit;
-            sigaction(SIGINT, &act2, NULL);
-            pause();
-        }
-    } else {
-        int cpid;
-        printf("Spawned child PID %d\n", (cpid = getpid()));
-        while (1) {
-            /*Random sleep from 1 to 5 seconds*/
-			sleepTime = rand() % 5 + 1;
-			printf("Waiting...\t");
-			fflush(stdout);
-			sleep(sleepTime);
-
-			/*Randomly raise signal*/
-			randSig = rand() % 2;
-			if(randSig == 0)
-			{
-				kill(cpid, SIGUSR1);
-			}else{
-				kill(cpid, SIGUSR2);
-			}
-        }
     }
 
+    if (pid && pid2) {
+            // parent
+        printf("Spawned child PID# %d\n", pid);
+        printf("Spawned child PID# %d\n", pid2);
+
+    }
+
+    // do the signal things
+    if (pid && pid2) {
+        // parent
+        sigaction(SIGINT, &act, NULL);
+        while (1) {
+            printf("Waiting...\t");
+            fflush(stdout);
+            pause();
+        }
+
+    }
+
+    else if (!pid || (pid && !pid2)) {
+        // children
+        while (1) {
+            sendsignal(ppid);
+        }
+    }
+        
     return 0;
 }
